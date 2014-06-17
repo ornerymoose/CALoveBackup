051b1a9740b256ddfb3d6f1783c66c625026d487
davidpardy <dpardy87@gmail.com>
2014-06-16 22:49:29 -0700

    trying no compression for js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
