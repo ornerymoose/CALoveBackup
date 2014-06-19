7d556080f22de1e3822898ac0c69b5ba1df297df
davidpardy <dpardy87@gmail.com>
2014-06-18 20:50:17 -0700

    added unchanged

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,9 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+  has_many :products
   
   def add_product(product_id, size, color, quantity)
-      current_item = line_items.build(product_id: product_id, size: size, color: color, quantity: quantity)
+    current_item = line_items.build(product_id: product_id, size: size, color: color, quantity: quantity)
   end
 
   def total_price
