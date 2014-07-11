52bc32b66cd70d62a55b027e2a1d6519985c66ca
davidpardy <dpardy87@gmail.com>
2014-07-11 16:53:52 -0700

    put welcome bg images as inline js to improve performance, removed from products.js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,17 +2,7 @@ $(function(){
   if ($('.top-bottom-padding').length == 0) {
     $('.checkout, .cart-table-header, .total_cell').hide();
   } 
-  
-	$('#products-welcome').backstretch("http://californialove.s3.amazonaws.com/rainbow2.jpg");
-	setTimeout(function(){
-		($('#products-welcome').backstretch([
-			"https://s3.amazonaws.com/californialove/oceanviewlight.jpg",
-			"https://s3.amazonaws.com/californialove/darkforest.jpg",
-			"https://s3.amazonaws.com/californialove/goldsunset.jpg",
-			"https://s3.amazonaws.com/californialove/mendo.jpg",
-			"http://californialove.s3.amazonaws.com/rainbow2.jpg"
-			], {duraction: 6000, fade: 2000}));
-	}, 6500);
+
 	$('.welcome-item-1, .product-item-1').backstretch("https://s3.amazonaws.com/californialove/womens_black_front.jpg");
 	$('.welcome-item-0, .product-item-0').backstretch("https://s3.amazonaws.com/californialove/mens_black_front.jpg");
 	$('.welcome-item-2, .product-item-2').backstretch("https://s3.amazonaws.com/californialove/twill_hat.jpg");
